cf53f76c5622afe1c54fa8f4247e94a6dca96cfe
Ihor Trehub <pptregubi@gmail.com>
2025-02-03 22:26:28 +0200

    Create fake UnitOfWork #35 Closed

---
+++ QuizGenerator/QuizGenerator.Model/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using QuizGenerator.Model.Entities;
+
+namespace QuizGenerator.Model.Interfaces;
+
+public interface IUnitOfWork
+{
+	public IRepository<Quiz> QuizRepository { get; }
+	public IRepository<Question> QuestionRepository { get; }
+	public IRepository<QuestionDetail> QuestionDetailRepository { get; }
+	public IRepository<AnswerDetail> AnswerDetailRepository { get; }
+
+	public Task SaveAsync();
+}
---
+++ QuizGenerator/QuizGenerator.Model/Models/Fakes/UnitOfWorkFake.cs
@@ -0,0 +1,29 @@
+﻿using QuizGenerator.Model.Entities;
+using QuizGenerator.Model.Interfaces;
+
+namespace QuizGenerator.Model.Models.Fakes;
+
+public class UnitOfWorkFake : IUnitOfWork
+{
+	private readonly TimeSpan _saveDelay;
+
+	public IRepository<Quiz> QuizRepository { get; }
+	public IRepository<Question> QuestionRepository { get; }
+	public IRepository<QuestionDetail> QuestionDetailRepository { get; }
+	public IRepository<AnswerDetail> AnswerDetailRepository { get; }
+
+	public UnitOfWorkFake(TimeSpan saveDelay, TimeSpan delay)
+	{
+		_saveDelay = saveDelay;
+
+		QuizRepository = new RepositoryFake<Quiz>(delay);
+		QuestionRepository = new RepositoryFake<Question>(delay);
+		QuestionDetailRepository = new RepositoryFake<QuestionDetail>(delay);
+		AnswerDetailRepository = new RepositoryFake<AnswerDetail>(delay);
+	}
+
+	public async Task SaveAsync()
+	{
+		await Task.Delay(_saveDelay);
+	}
+}
